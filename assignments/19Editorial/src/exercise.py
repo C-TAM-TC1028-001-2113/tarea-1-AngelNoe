31edb640a53ec83c02d6d844ddc635cd4dcf0989
AngelNoe <89592815+AngelNoe@users.noreply.github.com>
2021-08-30 10:15:17 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,12 +1,10 @@
 import math
 def main():
     # escribe tu código abajo de esta línea
-    pass
-palabras=int(input('Dame el número de palabras:'))
-paginas=math.ceil(palabras/475)
-descuento=(paginas*60)*.1
-total=(paginas*60)-descuento
-print('El costo de la publicacion es:',total)
-
+    palabras=int(input('Dame el número de palabras:'))
+    paginas=math.ceil(palabras/475)
+    descuento=(paginas*60)*.1
+    total=(paginas*60)-descuento
+    print('El costo de la publicacion es:',total)
 if __name__ == '__main__':
     main()
