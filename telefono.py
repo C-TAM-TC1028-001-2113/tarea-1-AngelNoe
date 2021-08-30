5b94d4016a06841f010158bc8744c288001d1a94
AngelNoe <89592815+AngelNoe@users.noreply.github.com>
2021-08-29 19:53:07 -0500

    Add files via upload
    todas las tareas

---
+++ telefono.py
@@ -0,0 +1,11 @@
+def main():
+    # escribe tu código abajo de esta línea
+    pass
+mensajes=int(input('Dame el numero de mensajes:'))
+megas=float(input('Dame el numero de megas:'))
+minutos=int(input('Dame el numero de minutos:'))
+total=(mensajes+megas+minutos)*0.80
+print('El costo mensual es:',total)
+
+if __name__ == '__main__':
+    main()
