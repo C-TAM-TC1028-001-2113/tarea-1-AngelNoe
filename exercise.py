5c621272eef29634cc1fd6eb5fbaaf47e4d84247
AngelNoe <89592815+AngelNoe@users.noreply.github.com>
2021-08-29 19:50:30 -0500

    Add files via upload

--- exercise.py
+++ exercise.py
@@ -1,12 +1,14 @@
-import math
 def main():
     # escribe tu código abajo de esta línea
     pass
-palabras=int(input('Dame el número de palabras:'))
-paginas=math.ceil(palabras/475)
-descuento=(paginas*60)*.1
-total=(paginas*60)-descuento
-print('El costo de la publicacion es:',total)
+x1= float(input('Dame x1: '))
+y1= float(input('Dame y1: '))
+x2= float(input('Dame x2: ')) 
+y2= float(input('Dame y2: '))
+
+pendiente=(y2-y1)/(x2-x1)
+
+print('Pendiente:', pendiente)
 
 if __name__ == '__main__':
     main()
